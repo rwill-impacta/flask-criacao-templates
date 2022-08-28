05d709fbc48bfc3919fe586b1b0e2140919e2b24
rwill-impacta <112194108+rwill-impacta@users.noreply.github.com>
2022-08-28 16:29:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('/saudacao')
+def saudacao():
+	return render_template('saudacao.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
